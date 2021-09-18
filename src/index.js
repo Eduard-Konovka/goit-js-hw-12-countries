a42ec851094833871f812947cb9d798123645c17
Eduard-Konovka <ed098ua@gmail.com>
2021-09-19 02:15:40 +0300

    Добавляет ДЗ12

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
-// ----------------------------------------------------------------------------
-// --------------------------------- ИМПОРТЫ ----------------------------------
-// ----------------------------------------------------------------------------
+import countriesListTpl from './templates/countriesListTpl.hbs'
+import countryCardTpl from './templates/countryCardTpl.hbs'
+import API from './js/fetchCountries'
+import getRefs from './js/refs'
 
-import db from './db/db.json'
-import templ from './templates/templ.hbs'
-import refs from './js/refs'
-import './js/func'
-import './js/listeners'
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js'
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js'
+import '@pnotify/core/dist/PNotify.css'
+import '@pnotify/core/dist/BrightTheme.css'
+defaultModules.set(PNotifyMobile, {})
+
+var debounce = require('lodash.debounce')
+
+const refs = getRefs()
+
+refs.searchForm.addEventListener('input', debounce(onSearch, 500))
+
+function onSearch(e) {
+  API.fetchCountry(e.target.value).then(quantityCheckCountries).catch(onFetchError)
+}
+
+function quantityCheckCountries(country) {
+  if (country.status === 404) {
+    refs.cardContainer.innerHTML = ''
+    alert({ text: 'Check the correctness of the data entered, this country does not exist!' })
+    return
+  } else if (country.length > 10) {
+    refs.cardContainer.innerHTML = ''
+    error({ text: 'Too many matches found. Please enter a more specific query!' })
+    return
+  } else if (country.length > 1) {
+    refs.cardContainer.innerHTML = countriesListTpl(country)
+    return
+  }
+  refs.cardContainer.innerHTML = countryCardTpl(country)
+}
+
+function onFetchError(err) {
+  refs.cardContainer.innerHTML = ''
+  alert({ text: 'Check the correctness of the data entered!' })
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,6 @@
-// ----------------------------------------------------------------------------
-// --------------------------------- ДОСТУПЫ ----------------------------------
-// ----------------------------------------------------------------------------
-
-export default []
+export default function getRefs() {
+  return {
+    cardContainer: document.querySelector('.card-container'),
+    searchForm: document.querySelector('.search-form'),
+  }
+}
